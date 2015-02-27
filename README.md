9c824b850cf611f241104c10ead593fd4f5e8374
Thomas Sundberg <tsu@kth.se>
2015-02-27 14:25:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Selenium and the Four Rules of Simple Design
+
+A Selenium example used for refactoring while considering The Four Rules of Simple Design
+
+The rules are: 
+
+* Tests passes
+* Express intent
+* No duplication
+* Small
+<<<<<<< HEAD
+=======
+
+>>>>>>> 2eb9a4c... Initial commit
---
+++ src/test/java/se/thinkcode/BuyGeekyStuffTest.java
@@ -0,0 +1,21 @@
+package se.thinkcode;
+
+import org.junit.Test;
+
+public class BuyGeekyStuffTest extends ThinkGeekHelper {
+
+    @Test
+    public void shouldBuySonicScrewDriver() throws Exception {
+        buySonicScrewDriver();
+    }
+
+    @Test
+    public void shouldBuyMineCraftHoodie() throws Exception {
+        buyMinecraftUniversityYouthHoodie();
+    }
+
+    @Test
+    public void shouldBuyFluxCapacitor() throws Exception {
+        buyBackToTheFutureFluxCapacitor();
+    }
+}
