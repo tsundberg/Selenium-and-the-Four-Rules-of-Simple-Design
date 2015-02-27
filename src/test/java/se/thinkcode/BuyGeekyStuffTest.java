2b197c2c4b344926468eff7ca84af7270df7525f
Thomas Sundberg <tsu@kth.se>
2015-02-27 18:35:50 +0100

    Simplify the example a bit

--- src/test/java/se/thinkcode/BuyGeekyStuffTest.java
+++ src/test/java/se/thinkcode/BuyGeekyStuffTest.java
@@ -5,16 +5,6 @@ import org.junit.Test;
 public class BuyGeekyStuffTest extends ThinkGeekHelper {
 
     @Test
-    public void shouldBuySonicScrewDriver() throws Exception {
-        buySonicScrewDriver();
-    }
-
-    @Test
-    public void shouldBuyMineCraftHoodie() throws Exception {
-        buyMinecraftUniversityYouthHoodie();
-    }
-
-    @Test
     public void shouldBuyFluxCapacitor() throws Exception {
         buyBackToTheFutureFluxCapacitor();
     }
--- src/test/java/se/thinkcode/ThinkGeekHelper.java
+++ src/test/java/se/thinkcode/ThinkGeekHelper.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
@@ -13,95 +15,17 @@ import static org.junit.Assert.assertThat;
 
 public class ThinkGeekHelper {
 
-    public void buySonicScrewDriver() {
+    public void buyBackToTheFutureFluxCapacitor() throws InterruptedException {
         WebDriver driver = new FirefoxDriver();
         try {
             driver.get("http://www.thinkgeek.com");
-            driver.findElement(By.linkText("Doctor Who")).click();
-
-            WebElement productListing = driver.findElement(By.xpath("html/body/div[3]/div[4]/div/div[2]/div[2]"));
-            List<WebElement> products = productListing.findElements(By.tagName("div"));
-            for (WebElement product : products) {
-                String productDescription = product.getText();
-                if (productDescription.startsWith("11th Doctor Sonic Screwdriver")) {
-                    product.findElement(By.tagName("a")).click();
-                    break;
-                }
-            }
-            driver.findElement(By.id("submitcart")).click();
-
-
-            driver.findElement(By.id("topnav_cart")).click();
-            driver.findElement(By.id("cart-table"));
-            List<WebElement> cartRows = driver.findElements(By.className("cart-table-row"));
-            assertThat(cartRows.size(), is(1));
-            WebElement productRow = cartRows.get(0);
-
-            WebElement productLink = productRow.findElement(By.partialLinkText("Sonic Screwdriver"));
-            assertNotNull(productLink);
-
-            WebElement quantityElement = productRow.findElement(By.name("0_qty"));
-            String quantity = quantityElement.getAttribute("value");
-            assertThat(quantity, is("1"));
-
-        } finally {
-            driver.quit();
-        }
-    }
-
-    public void buyMinecraftUniversityYouthHoodie() {
-        WebDriver driver = new FirefoxDriver();
-        try {
-            driver.get("http://www.thinkgeek.com");
-            driver.findElement(By.linkText("Minecraft")).click();
-
-            WebElement productListing = driver.findElement(By.xpath("html/body/div[3]/div[4]/div/div[2]/div[2]"));
-            List<WebElement> products = productListing.findElements(By.tagName("div"));
-            for (WebElement product : products) {
-                String productDescription = product.getText();
-                if (productDescription.startsWith("Minecraft University Youth Hoodie")) {
-                    product.findElement(By.tagName("a")).click();
-                    break;
-                }
-            }
-            driver.findElement(By.id("submitcart")).click();
-
-
-            driver.findElement(By.id("topnav_cart")).click();
-            driver.findElement(By.id("cart-table"));
-            List<WebElement> cartRows = driver.findElements(By.className("cart-table-row"));
-            assertThat(cartRows.size(), is(1));
-            WebElement productRow = cartRows.get(0);
-
-            WebElement productLink = productRow.findElement(By.partialLinkText("Youth Hoodie"));
-            assertNotNull(productLink);
-
-            WebElement quantityElement = productRow.findElement(By.name("0_qty"));
-            String quantity = quantityElement.getAttribute("value");
-            assertThat(quantity, is("1"));
-
-        } finally {
-            driver.quit();
-        }
-
-    }
-
-    public void buyBackToTheFutureFluxCapacitor() {
-        WebDriver driver = new FirefoxDriver();
-        try {
             driver.get("http://www.thinkgeek.com/interests/back-to-the-future/");
 
-            WebElement productListing = driver.findElement(By.xpath("html/body/div[3]/div[4]/div/div[2]/div[2]"));
-            List<WebElement> products = productListing.findElements(By.tagName("div"));
-            for (WebElement product : products) {
-                String productDescription = product.getText();
-                if (productDescription.startsWith("Back to the Future Flux Capacitor")) {
-                    product.findElement(By.tagName("a")).click();
-                    break;
-                }
-            }
-            driver.findElement(By.id("submitcart")).click();
+            WebDriverWait wait = new WebDriverWait(driver, 20);
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("item-1e2e")));
+            driver.findElement(By.id("item-1e2e")).findElement(By.tagName("a")).click();
 
+            driver.findElement(By.id("submitcart")).click();
 
             driver.findElement(By.id("topnav_cart")).click();
             driver.findElement(By.id("cart-table"));
