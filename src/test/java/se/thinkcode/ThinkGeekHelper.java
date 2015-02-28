21aed4e1b75070c693f3ab302375bf4db9c46cd7
Thomas Sundberg <tsu@kth.se>
2015-02-28 11:36:08 +0100

    Duplication - extracted common logic for asserting the cart

--- src/test/java/se/thinkcode/ThinkGeekHelper.java
+++ src/test/java/se/thinkcode/ThinkGeekHelper.java
@@ -32,17 +32,10 @@ public class ThinkGeekHelper {
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("topnav_cart")));
         browser.findElement(By.id("topnav_cart")).click();
-        browser.findElement(By.id("cart-table"));
-        List<WebElement> cartRows = browser.findElements(By.className("cart-table-row"));
-        assertThat(cartRows.size(), is(1));
-        WebElement productRow = cartRows.get(0);
 
-        WebElement productLink = productRow.findElement(By.partialLinkText("Flux Capacitor"));
-        assertNotNull(productLink);
 
-        WebElement quantityElement = productRow.findElement(By.name("0_qty"));
-        String quantity = quantityElement.getAttribute("value");
-        assertThat(quantity, is("1"));
+        String partialProductName = "Flux Capacitor";
+        assertCart(partialProductName);
     }
 
     public void addSonicScrewdriverToCart() throws InterruptedException {
@@ -57,12 +50,19 @@ public class ThinkGeekHelper {
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("topnav_cart")));
         browser.findElement(By.id("topnav_cart")).click();
+
+
+        String partialProductName = "Sonic Screwdriver";
+        assertCart(partialProductName);
+    }
+
+    private void assertCart(String partialProductName) {
         browser.findElement(By.id("cart-table"));
         List<WebElement> cartRows = browser.findElements(By.className("cart-table-row"));
         assertThat(cartRows.size(), is(1));
         WebElement productRow = cartRows.get(0);
 
-        WebElement productLink = productRow.findElement(By.partialLinkText("Sonic Screwdriver"));
+        WebElement productLink = productRow.findElement(By.partialLinkText(partialProductName));
         assertNotNull(productLink);
 
         WebElement quantityElement = productRow.findElement(By.name("0_qty"));
