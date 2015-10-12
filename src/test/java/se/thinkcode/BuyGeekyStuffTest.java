95b58ac733e425bc76ddb7ac32fa9d9e6a526ea4
Thomas Sundberg <tsu@kth.se>
2015-10-12 13:48:21 +0200

    Increased clarity by fixing test the test names

--- src/test/java/se/thinkcode/BuyGeekyStuffTest.java
+++ src/test/java/se/thinkcode/BuyGeekyStuffTest.java
@@ -7,17 +7,17 @@ public class BuyGeekyStuffTest {
     private GeekTestHelper geekTestHelper = new GeekTestHelper();
 
     @Test
-    public void shouldBuyFluxCapacitor() throws Exception {
+    public void shouldPlaceFluxCapacitorInShoppingBag() throws Exception {
         geekTestHelper.buyFluxCapacitor();
     }
 
     @Test
-    public void shouldBuyDrWhoSonicScrewdriver() throws Exception {
+    public void shouldPlaceDrWhoSonicScrewdriverInShoppingBag() throws Exception {
         geekTestHelper.buySonicScrewDriver();
     }
 
     @Test
-    public void shouldBuyGameOfThronesShootGlass() throws Exception {
+    public void shouldPlaceGameOfThronesShootGlassInShoppingBag() throws Exception {
         geekTestHelper.buyGameOfThronsShotGlasses();
     }
 }
