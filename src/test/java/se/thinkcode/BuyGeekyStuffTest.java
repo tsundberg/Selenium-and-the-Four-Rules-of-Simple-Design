03931435b9bfa29d2371272c50d0772ad4f8b978
Thomas Sundberg <tsu@kth.se>
2015-10-12 14:23:14 +0200

    Removed the test geek helper, use page objects instead

--- src/test/java/se/thinkcode/BuyGeekyStuffTest.java
+++ src/test/java/se/thinkcode/BuyGeekyStuffTest.java
@@ -3,17 +3,20 @@ package se.thinkcode;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.assertNotNull;
 
 public class BuyGeekyStuffTest {
     private WebDriver browser;
-    private GeekTestHelper geekTestHelper;
 
     @Before
     public void setUp() {
         browser = new FirefoxDriver();
-        geekTestHelper = new GeekTestHelper(browser);
     }
 
     @After
@@ -23,16 +26,52 @@ public class BuyGeekyStuffTest {
 
     @Test
     public void shouldPlaceFluxCapacitorInShoppingBag() throws Exception {
-        geekTestHelper.buyFluxCapacitor();
+        browser.get("http://www.thinkgeek.com");
+        browser.get("http://www.thinkgeek.com/interests/back-to-the-future");
+
+        String itemId = "item-1e2e";
+
+        ProductPage productPage = new ProductPage(browser);
+        WebDriverWait wait = productPage.locateItem(itemId);
+        WebElement productRow = productPage.addItemToShoppingCart(wait);
+
+        WebElement productLink = productRow.findElement(By.partialLinkText("Flux Capacitor"));
+        assertNotNull(productLink);
+        ShoppingCartPage shoppingCartPage = new ShoppingCartPage();
+        shoppingCartPage.verifyOneItemInShoppingCart(productRow);
     }
 
     @Test
     public void shouldPlaceDrWhoSonicScrewdriverInShoppingBag() throws Exception {
-        geekTestHelper.buySonicScrewDriver();
+        browser.get("http://www.thinkgeek.com");
+        browser.get("http://www.thinkgeek.com/interests/doctorwho");
+
+        String itemId = "item-ee4a";
+        ProductPage productPage = new ProductPage(browser);
+        WebDriverWait wait = productPage.locateItem(itemId);
+        WebElement productRow = productPage.addItemToShoppingCart(wait);
+
+        WebElement productLink = productRow.findElement(By.partialLinkText("Sonic Screwdriver"));
+        assertNotNull(productLink);
+
+        ShoppingCartPage shoppingCartPage = new ShoppingCartPage();
+        shoppingCartPage.verifyOneItemInShoppingCart(productRow);
     }
 
     @Test
     public void shouldPlaceGameOfThronesShootGlassInShoppingBag() throws Exception {
-        geekTestHelper.buyGameOfThronsShotGlasses();
+        browser.get("http://www.thinkgeek.com");
+        browser.get("http://www.thinkgeek.com/interests/gameofthrones");
+
+        String itemId = "item-f3c0";
+        ProductPage productPage = new ProductPage(browser);
+        WebDriverWait wait = productPage.locateItem(itemId);
+        WebElement productRow = productPage.addItemToShoppingCart(wait);
+
+        WebElement productLink = productRow.findElement(By.partialLinkText("Game of Thrones Shot Glass Set"));
+        assertNotNull(productLink);
+
+        ShoppingCartPage shoppingCartPage = new ShoppingCartPage();
+        shoppingCartPage.verifyOneItemInShoppingCart(productRow);
     }
 }
