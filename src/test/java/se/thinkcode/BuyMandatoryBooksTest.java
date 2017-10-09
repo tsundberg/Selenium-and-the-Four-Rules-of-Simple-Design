05167d503c852e671334719ef97e057be140b549
Thomas Sundberg <tsu@kth.se>
2017-10-09 11:02:27 +0200

    Small - extract search product

--- src/test/java/se/thinkcode/BuyMandatoryBooksTest.java
+++ src/test/java/se/thinkcode/BuyMandatoryBooksTest.java
@@ -30,14 +30,11 @@ public class BuyMandatoryBooksTest {
 
     @Test
     public void put_working_effectively_with_legacy_code_in_shopping_bag() throws Exception {
-        WebDriverWait wait = new WebDriverWait(browser, 20);
-        WebElement searchBox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("twotabsearchtextbox")));
-
         String searchString = "Working Effectively with Legacy Code";
-        searchBox.sendKeys(searchString);
-        WebElement searchButton = browser.findElement(By.id("nav-search-submit-text"));
 
-        searchButton.click();
+        WebDriverWait wait = new WebDriverWait(browser, 20);
+
+        searchProduct(searchString, wait);
 
         WebElement resultList = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("s-results-list-atf")));
 
@@ -67,6 +64,15 @@ public class BuyMandatoryBooksTest {
         assertThat(htmlClass).containsIgnoringCase("a-alert-success");
 
         browser.quit();
+    }
+
+    private void searchProduct(String searchString, WebDriverWait wait) {
+        WebElement searchBox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("twotabsearchtextbox")));
+
+        searchBox.sendKeys(searchString);
+        WebElement searchButton = browser.findElement(By.id("nav-search-submit-text"));
+
+        searchButton.click();
     }
 
 }
