69435468ff5e09788be8288d83442e114dd8e0d6
Thomas Sundberg <tsu@kth.se>
2018-04-14 14:31:53 +0300

    Express intent - hide the details for adding and searching

--- src/test/java/se/thinkcode/BuyMandatoryBooksTest.java
+++ src/test/java/se/thinkcode/BuyMandatoryBooksTest.java
@@ -40,12 +40,16 @@ public class BuyMandatoryBooksTest {
     public void put_working_effectively_with_legacy_code_in_shopping_bag() {
         String searchString = "Working Effectively with Legacy Code";
 
-        searchProduct(searchString);
-
-        WebElement theBook = locateProduct(searchString);
+        WebElement theBook = findProduct(searchString);
         WebElement itemInShoppingBag = addToBag(theBook);
 
         assertThatProductIsInBag(itemInShoppingBag);
+    }
+
+    private WebElement findProduct(String searchString) {
+        searchProduct(searchString);
+
+        return locateProduct(searchString);
     }
 
     private void assertThatProductIsInBag(WebElement itemInShoppingBag) {
