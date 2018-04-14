ad97b0167283682ab725f5e6a7e850ac2c2a19b2
Thomas Sundberg <tsu@kth.se>
2018-04-14 10:35:17 +0300

    No need to start the example with a warning from Idea

--- src/test/java/se/thinkcode/BuyMandatoryBooksTest.java
+++ src/test/java/se/thinkcode/BuyMandatoryBooksTest.java
@@ -17,7 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class BuyMandatoryBooksTest {
 
     @Test
-    public void shouldBuyWorkingEffectivelyWithLegacyCode() throws Exception {
+    public void shouldBuyWorkingEffectivelyWithLegacyCode() {
         URL url = getClass().getResource("/geckodriver");
         String geckoDriverPath = url.getFile();
         System.setProperty("webdriver.gecko.driver", geckoDriverPath);
