b7d04438247e4fdfc7317c6f78855e647437a184
Thomas Sundberg <tsu@kth.se>
2018-06-08 13:07:27 +0300

    Small - extract a page object and hide all navigation

---
+++ src/test/java/se/thinkcode/AmazonHelper.java
@@ -0,0 +1,69 @@
+package se.thinkcode;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AmazonHelper {
+    private WebDriverWait wait;
+
+    AmazonHelper(WebDriver browser) {
+        wait = new WebDriverWait(browser, 20);
+    }
+
+    WebElement findProduct(String searchString) {
+        searchProduct(searchString);
+
+        return locateProduct(searchString);
+    }
+
+    void assertThatProductIsInShoppingBag(WebElement itemInShoppingBag) {
+        String htmlClass = itemInShoppingBag.getAttribute("class");
+
+        assertThat(htmlClass).containsIgnoringCase("a-alert-success");
+    }
+
+    WebElement addProductToShoppingBag(WebElement theBook) {
+        theBook.click();
+
+        WebElement addToCartButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("add-to-cart-button")));
+        addToCartButton.click();
+
+
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("huc-v2-order-row-icon")));
+    }
+
+    private WebElement locateProduct(String searchString) {
+        WebElement resultList = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("s-results-list-atf")));
+
+        WebElement theBook = null;
+
+        List<WebElement> resultListElements = resultList.findElements(By.className("s-access-detail-page"));
+
+        for (WebElement element : resultListElements) {
+            String title = element.getAttribute("title");
+            if (title.contains(searchString)) {
+                theBook = element;
+                break;
+            }
+        }
+
+        assertThat(theBook).isNotNull();
+        return theBook;
+    }
+
+    private void searchProduct(String searchString) {
+        WebElement searchBox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("twotabsearchtextbox")));
+
+        searchBox.sendKeys(searchString);
+        searchBox.sendKeys(Keys.RETURN);
+    }
+
+}
--- src/test/java/se/thinkcode/BuyMandatoryBooksTest.java
+++ src/test/java/se/thinkcode/BuyMandatoryBooksTest.java
@@ -3,23 +3,15 @@ package se.thinkcode;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class BuyMandatoryBooksTest {
-
     private WebDriver browser;
-    private WebDriverWait wait;
+    private AmazonHelper helper;
 
     @Before
     public void setUp() {
@@ -29,7 +21,7 @@ public class BuyMandatoryBooksTest {
         browser = new FirefoxDriver();
 
         browser.get("http://www.amazon.de");
-        wait = new WebDriverWait(browser, 20);
+        helper = new AmazonHelper(browser);
     }
 
     @After
@@ -40,59 +32,12 @@ public class BuyMandatoryBooksTest {
     @Test
     public void put_working_effectively_with_legacy_code_in_shopping_bag() {
         String searchString = "Working Effectively with Legacy Code";
-        WebElement theBook = findProduct(searchString);
+        WebElement theBook = helper.findProduct(searchString);
 
-        WebElement itemInShoppingBag = addProductToShoppingBag(theBook);
+        WebElement itemInShoppingBag = helper.addProductToShoppingBag(theBook);
 
-        assertThatProductIsInShoppingBag(itemInShoppingBag);
+        helper.assertThatProductIsInShoppingBag(itemInShoppingBag);
     }
 
-    private WebElement findProduct(String searchString) {
-        searchProduct(searchString);
-
-        return locateProduct(searchString);
-    }
-
-    private void assertThatProductIsInShoppingBag(WebElement itemInShoppingBag) {
-        String htmlClass = itemInShoppingBag.getAttribute("class");
-
-        assertThat(htmlClass).containsIgnoringCase("a-alert-success");
-    }
-
-    private WebElement addProductToShoppingBag(WebElement theBook) {
-        theBook.click();
-
-        WebElement addToCartButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("add-to-cart-button")));
-        addToCartButton.click();
-
-
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("huc-v2-order-row-icon")));
-    }
-
-    private WebElement locateProduct(String searchString) {
-        WebElement resultList = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("s-results-list-atf")));
-
-        WebElement theBook = null;
-
-        List<WebElement> resultListElements = resultList.findElements(By.className("s-access-detail-page"));
-
-        for (WebElement element : resultListElements) {
-            String title = element.getAttribute("title");
-            if (title.contains(searchString)) {
-                theBook = element;
-                break;
-            }
-        }
-
-        assertThat(theBook).isNotNull();
-        return theBook;
-    }
-
-    private void searchProduct(String searchString) {
-        WebElement searchBox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("twotabsearchtextbox")));
-
-        searchBox.sendKeys(searchString);
-        searchBox.sendKeys(Keys.RETURN);
-    }
 
 }
